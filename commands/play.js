c14369c225a7a83d07cc3178ae3058fa68137d99
Dxboy <61777823+Dxboy@users.noreply.github.com>
2021-01-27 13:12:34 +0800

    added processes

--- commands/play.js
+++ commands/play.js
@@ -7,5 +7,10 @@ module.exports = {
     aliases: ['p'], //optional...leave the array empty
     async execute(client, message, args) {
         client.distube.play(message, args.join(' '))
+        const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
+	    client.distube
+    	    .on("playSong", (message, queue, song) => message.channel.send(
+    	        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    	    ))
     }
 }
